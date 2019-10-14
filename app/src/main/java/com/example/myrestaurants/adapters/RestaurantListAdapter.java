66320389cb3f6e455e7b2020d24421d5b51c6a89
Robert Muthuri <muthuri.r@gmail.com>
2019-10-14 11:10:15 +0300

    Adds an inner/nested class for the viewholder with restaurant viewholder and bind methods.

--- app/src/main/java/com/example/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/example/myrestaurants/adapters/RestaurantListAdapter.java
@@ -1,12 +1,19 @@
 package com.example.myrestaurants.adapters;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myrestaurants.R;
 import com.example.myrestaurants.models.Business;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
     private List<Business> mRestaurants;
@@ -14,5 +21,27 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
     public RestaurantListAdapter(Context context, List<Business> restaurants) {
         mContext = context;
         mRestaurants = restaurants;
+    }
+    // inner class/nested class for the view holder.
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.restaurantImageView) ImageView mRestaurantImageView;
+        @BindView(R.id.restaurantNameTextView) TextView mNameTextView;
+        @BindView(R.id.categoryTextView) TextView mCategoryTextView;
+        @BindView(R.id.ratingTextView) TextView mRatingTextView;
+
+        private Context mContext;
+
+        public RestaurantViewHolder (View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindRestaurant(Business restaurant) {
+            mNameTextView.setText(restaurant.getName());
+            mCategoryTextView.setText(restaurant.getCategories().get(0).getTitle());
+            mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        }
+
     }
 }
