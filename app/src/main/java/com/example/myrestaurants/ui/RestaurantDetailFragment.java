e65b3e15b0f7b8b508503fa7947bd8c01d90ea74
Robert Muthuri <muthuri.r@gmail.com>
2019-10-15 17:39:16 +0300

    Instantiates the a restaurant-detail-fragment, unwraps the restaurant object in the on-create method, and used to set views in the on-create-view method.

--- app/src/main/java/com/example/myrestaurants/ui/RestaurantDetailFragment.java
+++ app/src/main/java/com/example/myrestaurants/ui/RestaurantDetailFragment.java
@@ -8,25 +8,75 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.myrestaurants.R;
+import com.example.myrestaurants.models.Business;
+import com.example.myrestaurants.models.Category;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RestaurantDetailFragment extends Fragment {
+    @BindView(R.id.restaurantImageView) ImageView mImageLabel;
+    @BindView(R.id.restaurantNameTextView) TextView mNameLabel;
+    @BindView(R.id.cuisineTextView) TextView mCategoriesLabel;
+    @BindView(R.id.ratingTextView) TextView mRatingLabel;
+    @BindView(R.id.websiteTextView) TextView mWebsiteLabel;
+    @BindView(R.id.phoneTextView) TextView mPhoneLabel;
+    @BindView(R.id.addressTextView) TextView mAddressLabel;
+    @BindView(R.id.saveRestaurantButton) Button mSaveRestaurantButton;
 
+    private Business mRestaurant;
 
     public RestaurantDetailFragment() {
         // Required empty public constructor
     }
 
-
+    public static RestaurantDetailFragment newInstance(Business restaurant) {
+        RestaurantDetailFragment restaurantDetailFragment = new RestaurantDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("restaurant", Parcels.wrap(restaurant));
+        restaurantDetailFragment.setArguments(args);
+        return restaurantDetailFragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRestaurant = Parcels.unwrap(getArguments().getParcelable("restaurant"));
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
+        ButterKnife.bind(this, view);
+        Picasso.get().load(mRestaurant.getImageUrl()).into(mImageLabel);
+
+        List<String> categories = new ArrayList<>();
+
+        for (Category category: mRestaurant.getCategories()) {
+            categories.add(category.getTitle());
+        }
+
+        mNameLabel.setText(mRestaurant.getName());
+        mCategoriesLabel.setText(android.text.TextUtils.join(", ", categories));
+        mRatingLabel.setText(Double.toString(mRestaurant.getRating()) + "/5");
+        mPhoneLabel.setText(mRestaurant.getPhone());
+        mAddressLabel.setText(mRestaurant.getLocation().toString());
+
+        return view;
     }
 
 }
