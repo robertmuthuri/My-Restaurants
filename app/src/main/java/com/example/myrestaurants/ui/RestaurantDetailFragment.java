f72a4dc0558b488e9d54ecfff71d091fc582397d
Robert Muthuri <muthuri.r@gmail.com>
2019-10-15 13:03:30 +0300

    Creates a restaurant-detail-fragment and it's corresponding fragment-restaurant-detail layout.

---
+++ app/src/main/java/com/example/myrestaurants/ui/RestaurantDetailFragment.java
@@ -0,0 +1,32 @@
+package com.example.myrestaurants.ui;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myrestaurants.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RestaurantDetailFragment extends Fragment {
+
+
+    public RestaurantDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
+    }
+
+}
