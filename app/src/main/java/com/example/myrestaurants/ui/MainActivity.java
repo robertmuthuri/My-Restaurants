cbb5d87fd10695e042795510419c7ee22bf71b3b
Robert Muthuri <muthuri.r@gmail.com>
2019-10-21 16:55:43 +0300

    Initialize shared preferences variables, editor and method to add shared preferences to the constant file.

--- app/src/main/java/com/example/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/ui/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.myrestaurants.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.myrestaurants.R;
+import com.example.myrestaurants.models.Constants;
 
 import org.parceler.Parcels;
 
@@ -17,6 +20,10 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    // Create member variables to store refs to shared preferences tool itself
+    private SharedPreferences mSharedPreferences;
+    // Create the dedicated tool to edit the foregoing variables.
+    private SharedPreferences.Editor mEditor;
 
 //  private Button mFindRestaurantsButton; //adds a member variable to hold our button so that we can access it inside all of our methods
     @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
@@ -29,6 +36,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);      // causes android to run all the default behaviours for an activity
         setContentView(R.layout.activity_main); // tells the activity which layout to use on the device screen
         ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
 
 //        mLocationEditText = (EditText) findViewById(R.id.locationEditText); //set the EditText variable
 //        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton); // set the button variable
@@ -55,5 +65,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show(); // makes a toast to confirm that zip location keyed in.
 
         }
+    }
+    // Create method which takes user input zip-code as argument
+    private void addToSharedPreferences(String location) {
+        // Provide editor with the key stored in Constants file and the zip code location passed as argument
+        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
     }
 }
