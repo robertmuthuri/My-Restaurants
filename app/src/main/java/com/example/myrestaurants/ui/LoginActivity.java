5f136a0e9e835a3f9d383b82f6ffc130c473e22b
Robert Muthuri <muthuri.r@gmail.com>
2019-10-27 20:53:21 +0300

    Adds a click listener to allow a user to create an account if they don't have one yet.

--- app/src/main/java/com/example/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/com/example/myrestaurants/ui/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.myrestaurants.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.myrestaurants.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView) TextView mRegisteredTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @Override public void onClick(View view) {
+        if (view == mRegisteredTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
