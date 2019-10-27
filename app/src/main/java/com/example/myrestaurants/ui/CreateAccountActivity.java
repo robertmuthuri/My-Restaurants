227571addb40f5b2ed25a20db4200f06041e76a5
Robert Muthuri <muthuri.r@gmail.com>
2019-10-27 21:04:47 +0300

    Basic setup of create account activity - member variable declaration, binding elements, and a placeholder function to create a new user.

--- app/src/main/java/com/example/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/myrestaurants/ui/CreateAccountActivity.java
@@ -1,16 +1,46 @@
 package com.example.myrestaurants.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.accessibility.AccessibilityViewCommand;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.myrestaurants.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.createUserButton) Button mCreateUserButton;
+    @BindView(R.id.nameEditText) EditText mEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView) Button mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
+    }
+
+    @Override public void onClick (View v) {
+        if (v == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (v == mCreateUserButton) {
+//            createNewUser();
+        }
     }
 }
