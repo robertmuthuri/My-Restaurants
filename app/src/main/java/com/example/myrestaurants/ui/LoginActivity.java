e9deacaf0c7cb5ea492560c5114eb6469fc75d19
Robert Muthuri <muthuri.r@gmail.com>
2019-10-28 14:20:35 +0300

    Adds Firebase object instance and binds views.

--- app/src/main/java/com/example/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/com/example/myrestaurants/ui/LoginActivity.java
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.myrestaurants.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.registerTextView) TextView mRegisteredTextView;
+    @BindView(R.id.passwordLoginButton) Button mpasswordLoginButton;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         ButterKnife.bind(this);
 
         mRegisteredTextView.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override public void onClick(View view) {
